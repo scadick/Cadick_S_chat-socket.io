456a8bc29a23a0daa1d42d4af9144ec556bddd2a
Cadick <s_cadick2@tss-hr420-dm07.local>
2018-01-29 13:32:10 -0500

    updating branch

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const io = require('socket.io')();
 
 app.use(express.static('public'));
 
@@ -8,6 +9,18 @@ app.use(require('./routes/index'));
 app.use(require('./routes/contact'));
 app.use(require('./routes/users'));
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('app running on port 3000!');
+});
+
+io.attach(server);
+
+io.on('connection', (socket) => {
+  console.log('a user has connected!');
+  io.emit('connectMsg', { for: 'everyone', msg: `${socket.id} is here!`});
+
+  socket.on('disconnect', () => {
+    console.log('a user has disconnected!');
+  });
+
 });
